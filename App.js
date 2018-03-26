52fd2369697bb2886fda3b4f3bbee74f690ad0aa
Andréas PASTOR <andreas.pastor@etu.univ-nantes.fr>
2018-03-26 21:23:54 +0200

    add react-native-audio-toolkit for sound recording & playing

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import ModalDropdown from 'react-native-modal-dropdown';
 
 window.navigator.userAgent = 'react-native';
 import SocketIOClient from 'socket.io-client';
 
+import {
+  Player,
+  Recorder,
+  MediaStates
+} from 'react-native-audio-toolkit';
+
 export default class App extends Component {
 
   state = {
     soundSource: 'Sound 0',
+    name: 'Laouen'
   }
 
   constructor() {
     super();
     this.socket = SocketIOClient('https://serversocket2018.herokuapp.com/');
-    this.socket.on('update', () => this.setState({name: 'None'}));
+    this.socket.on('update', () => this.setState({name: 'Felix'}));
   }
 
   onSelect(i, v) {
@@ -23,11 +30,40 @@ export default class App extends Component {
     })
   }
 
+  _onPress() {
+    // Disable button while recording and playing back
+    this.setState({disabled: true});
+  
+    // Start recording
+    let rec = new Recorder("filename.mp4").record();
+  
+    // Stop recording after approximately 3 seconds
+    setTimeout(() => {
+      rec.stop((err) => {
+        // NOTE: In a real situation, handle possible errors here
+  
+        // Play the file after recording has stopped
+        new Player("filename.mp4")
+        .play()
+        .on('ended', () => {
+          // Enable button again after playback finishes
+          this.setState({disabled: false});
+        });
+      });
+    }, 3000);
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <TouchableHighlight disabled={this.state.disabled} onPress={() => this._onPress()}>
+          <Text>
+            Press me!
+          </Text>
+        </TouchableHighlight>
         <ModalDropdown onSelect={this.onSelect.bind(this)} options={['Sound 1', 'Sound 2', 'Sound 3', 'Sound 4']}/>
         <Text>{this.state.soundSource}</Text>
+        <Text>{this.state.name}</Text>
       </View>
     );
   }
